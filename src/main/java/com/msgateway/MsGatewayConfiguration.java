3bb62a101b4cc5d42ccdb5e2a905517b3d44dc84
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2021-04-26 22:00:52 -0300

    Created project

---
+++ src/main/java/com/msgateway/MsGatewayConfiguration.java
@@ -0,0 +1,39 @@
+package com.msgateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MsGatewayConfiguration {
+	
+	@Value("${URI_INDEX_USERS}")
+	private String apiIndex;	
+	
+	@Value("${URI_MICROSERVICE}")
+	private String uriMicroservice;	
+	
+	@Value("${URI_AUTHENTICATE}")
+	private String uriAuthenticate;
+	
+	@Value("${URI_USERS}")
+	private String uriUsers;
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+				// Route to /api/index in the ms-auth microservice 
+				.route(p -> p.path(apiIndex)			
+						.uri(uriMicroservice))	
+				// Route to /authentication in the ms-auth microservice 
+				.route(p -> p.path(uriAuthenticate)
+						.uri(uriMicroservice))	
+				// Route to /apiusers/** in the ms-auth microservice - GET | POST | PUT | DELETE
+				.route(p -> p.path(uriUsers)
+						.uri(uriMicroservice))	
+		.build();
+	}
+}
